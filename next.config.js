75c250d474af38efdd182065dc5e726f19361915
Suresh <singh.suresh318@gmail.com>
2023-05-01 23:13:58 +0530

    webpack

--- next.config.js
+++ next.config.js
@@ -4,12 +4,15 @@ const nextConfig = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback.fs = false;
+    } else {
+      // Add the following line to exclude fs module from being bundled in the server-side build
+      config.externals.push({ fs: 'empty' });
     }
-
     return config;
   },
 };
 
 module.exports = {
   pageExtensions: ['jsx', 'js', 'ts', 'tsx', 'component.js'],
-}
+  ...nextConfig,
+};
