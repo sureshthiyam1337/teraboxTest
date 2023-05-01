cadc00ec3ad1427d9afb8287c3d4b54c483f5640
Suresh <singh.suresh318@gmail.com>
2023-05-01 22:36:20 +0530

    updated telegram bot link

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true
-}
-
-module.exports = nextConfig
+module.exports = {
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
+};
