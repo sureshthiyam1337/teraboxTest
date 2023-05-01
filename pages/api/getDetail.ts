1d8c0fe1e398129d7fb920b238bd7cbca33729cd
Suresh <singh.suresh318@gmail.com>
2023-05-01 19:14:10 +0530

    updated telegram bot link

--- pages/api/getDetail.ts
+++ pages/api/getDetail.ts
@@ -86,3 +86,4 @@ export default async function handler(
         return
     }
 }
+export default getDetail;
--- pages/api/getDownloadUrl.ts
+++ pages/api/getDownloadUrl.ts
@@ -81,3 +81,4 @@ export default async function handler(
     }
 
 }
+export default getDownloadUrl;
